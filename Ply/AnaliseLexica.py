4f0557cfac40b41ba4731b6a528dd07a85fb9dce
luizmoitinho <nippecufs@gmail.com>
2019-12-01 00:24:42 -0300

    Feature -  Inclusão de palavras reservas, tokens e regras

--- Ply/AnaliseLexica.py
+++ Ply/AnaliseLexica.py
@@ -7,16 +7,42 @@
 import ply.lex as lex;
 
 reserved = {
+    'include':'INCLUDE',
+    'require':'REQUIRE',
+    'require_once':'REQUIRE_ONCE',
+    'function':'FUNCTION',
+    'use':'USE',
+    'as':'AS',
     'if' : 'IF',
     'else' : 'ELSE',
+    'endif':'ENDIF',
+    'switch':'SWITCH',
+    'case':'CASE',
+    'break':'BREAK',
+    'continue':'CONTINUE',
+    'echo':'ECHO',
+    'var_dump':'VAR_DUMP',
+    'true':'TRUE',
+    'false':'FALSE',
     'for' : 'FOR',
     'while' : 'WHILE',
+    'do':'DO'
+
 }
+
 tokens =[
+    'BEGIN_PROGRAM',
+    'END_PROGRAM',
     'NUMBER_INTEGER',
     'ID',
     'NUMBER_REAL',
     'ASSIGN',
+    'INCREMENT',
+    'DECREMENT',
+    'POS_INCREMENT',
+    'PRE_INCREMENT',
+    'POS_DECREMENT',
+    'PRE_DECREMENT',
     'PLUS',
     'MINUS',
     'TIMES',
@@ -38,12 +64,21 @@ tokens =[
     'TAB'] + list(reserved.values())
 
 #EXPRESSOES REGULARES: No ply é indenficado usando o sufixo t_[nome do token] 
-        #operadores aritméticos
+       
 
 t_TAB = r'\t'
-t_COMMENT_LINE = r'\//.*'
+t_COMMENT_LINE = r'\//.* | \#.*'
 t_COMMENT_LINE2 = r'\/\*(.|\n)*\*\/'
+
+        #operadores aritméticos
 t_ASSIGN =  r'\='
+t_INCREMENT =  r'\+\+'
+t_DECREMENT =  r'\-\-'
+t_POS_INCREMENT = r'\+\+'
+t_PRE_INCREMENT = r'\+\='
+t_POS_DECREMENT = r'\=\-'
+t_PRE_DECREMENT = r'\-\='
+
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
@@ -59,6 +94,8 @@ t_EQUAL = r'\=\='
 t_NOT_EQUAL = r'\!\='
 
 
+t_BEGIN_PROGRAM =  r'\<\?php | \<\?\= | \<\?'
+t_END_PROGRAM   =  r'\?\>'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_LKEY = r'\{'
@@ -71,10 +108,10 @@ t_ignore = ' '
 
 #EXPRESSOES REGULARES COM FUNÇÕES
 def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    r'\$[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value,'ID')
     return t
-
+    
 def t_NUMBER_REAL(t):
     r'\d*\.\d+'
     t.value = float(t.value)
@@ -97,7 +134,7 @@ def t_error(t):
 
 
 #Execução do lexico
-arquivo =  'function if  else for while/*AB\nljljffdgdlkjlkjC*/D(5555){ int = a; //echo\n(a)}';
+arquivo = "-="
 lexer = lex.lex()
 
 #inputa o arquivo no classe lexer para gerar os tokens
