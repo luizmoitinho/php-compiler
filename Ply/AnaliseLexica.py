94f32215d36b3782e152b68a4dca86fc959ca178
WedsonTeixeira <wedson.teixeira1998@outlook.com>
2019-11-30 22:17:09 -0300

    Adicionando Comentarios e identificadores

--- Ply/AnaliseLexica.py
+++ Ply/AnaliseLexica.py
@@ -6,10 +6,15 @@
 
 import ply.lex as lex;
 
-# LISTA DE NOME DOS TOKENS
-tokens = (
-
+reserved = {
+    'if' : 'IF',
+    'else' : 'ELSE',
+    'for' : 'FOR',
+    'while' : 'WHILE',
+}
+tokens =[
     'NUMBER_INTEGER',
+    'ID',
     'NUMBER_REAL',
     'ASSIGN',
     'PLUS',
@@ -21,48 +26,54 @@ tokens = (
     'RPAREN',
     'LKEY',
     'RKEY',
-    'BEGIN_COMMENT',
-    'END_COMMENT',
     'COMMENT_LINE',
+    'COMMENT_LINE2',
     'LESS_THAN',
     'LESS_EQUAL',
     'GREAT_THAN',
     'GREAT_EQUAL',
     'EQUAL',
-    'NOT_EQUAL'
-);
+    'NOT_EQUAL',
+    'SEMICOLON',
+    'TAB'] + list(reserved.values())
 
 #EXPRESSOES REGULARES: No ply é indenficado usando o sufixo t_[nome do token] 
         #operadores aritméticos
+
+t_TAB = r'\t'
+t_COMMENT_LINE = r'\//.*'
+t_COMMENT_LINE2 = r'\/\*(.|\n)*\*\/'
 t_ASSIGN =  r'\='
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
 t_DIVIDE = r'/'
 t_PERCENT = r'\%'
+
         #operadores relacionais
 t_LESS_THAN = r'\<'
 t_GREAT_THAN =  r'\>'
-
 t_LESS_EQUAL = r'\<\='
 t_GREAT_EQUAL = r'\>\='
-
 t_EQUAL = r'\=\='
 t_NOT_EQUAL = r'\!\='
 
 
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
-t_LKEY = r'\{}'
-t_RKEY = r'}'
-t_COMMENT_LINE=r'\//'
+t_LKEY = r'\{'
+t_RKEY = r'\}'
+t_SEMICOLON = r'\;'
+t_ignore = ' '
 
 
 
 
 #EXPRESSOES REGULARES COM FUNÇÕES
-def t_IDENTIFIER(t):
-    r'\[a-ZA-Z]'
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')
+    return t
 
 def t_NUMBER_REAL(t):
     r'\d*\.\d+'
@@ -74,14 +85,9 @@ def t_NUMBER_INTEGER(t):
     t.value = int(t.value) # Faz o cast do atributo value para inteiro.
     return t
 
-
-
 def t_newline(t):
     r'\n+'
     pass
-
-t_ignore = ' \t'
-
 
 #Regra de tratamento de erros
 def t_error(t):
@@ -91,7 +97,7 @@ def t_error(t):
 
 
 #Execução do lexico
-arquivo =  'x=10.20';
+arquivo =  'function if  else for while/*AB\nljljffdgdlkjlkjC*/D(5555){ int = a; //echo\n(a)}';
 lexer = lex.lex()
 
 #inputa o arquivo no classe lexer para gerar os tokens
