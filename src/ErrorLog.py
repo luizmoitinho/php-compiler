7ff85e11de93099355203fd1be95fa5afc1a8da7
Erick Oliveira <erickoliveiraah@gmail.com>
2020-06-09 17:46:54 -0300

    Adicionar mensagem de erro

--- src/ErrorLog.py
+++ src/ErrorLog.py
@@ -5,6 +5,12 @@ def AttributionTypeError(self, assignExpr, exprType):
   assignExpr.variable.accept(self.printer)
   print(' returned type', exprType)
   
+def AttributionInvalidTypeError(self, exprType, assignExpr, bindable):
+  print('ERROR: Invalid atribution of type', exprType, end='') 
+  print(' on variable ', end='') 
+  assignExpr.variable.accept(self.printer)
+  print(' that has type', bindable[st.TYPE])
+  
 def ExpressionTypeError(self, expr, type1, type2):
   print('ERROR: Expression ', end='')
   expr.expr1.accept(self.printer)
